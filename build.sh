86feb34613b334908e33087878cd43de52a48b5d
Conner Brooks <connerbrooks@gmail.com>
2014-07-23 13:42:13 -0700

    Update build scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,18 @@
+#!/bin/bash
+
 # build site and deploy to gh-pages
 
+hugo
+
+git add -A
+
+msg="Rebuilding site `date`"
+if [ $# -eq 1  ]
+    then msg="$1"
+fi
+git commit -m "$msg"
+
+# Subtree for gh-pages
 git subtree split --prefix public -b gh-pages
 git push -f origin gh-pages:gh-pages
 git branch -D gh-pages
